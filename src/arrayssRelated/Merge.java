39a751942f9ac67dc6e9911ebaec0cffee8f8899
Md Saheed Parvez <saheed.parvez@i-exceed.com>
2026-01-19 12:59:51 +0530

    Fixed merge issue

--- src/arrayssRelated/Merge.java
+++ src/arrayssRelated/Merge.java
@@ -6,17 +6,25 @@ public class Merge {
 
 	public static void main(String[] args) {
 
-		int a1[] = {1,2,5,7,6};  int m=3;
-		int a2[] = {5,6,8};      int n= 3;
-		int i,j = 0;
-		
-		for( j=0, i=m; j<n; j++) {
+		int a1[] = new int[6]; // enough space
+		a1[0] = 1;
+		a1[1] = 2;
+		a1[2] = 5;
+
+		int m = 3;
+
+		int a2[] = { 5, 6, 8 };
+		int n = 3;
+
+		int i = m;
+
+		for (int j = 0; j < n; j++) {
 			a1[i] = a2[j];
 			i++;
 		}
-		Arrays.sort(a1);
-		System.out.println(a1);
-		
-	}
 
+		Arrays.sort(a1);
+
+		System.out.println(Arrays.toString(a1));
+	}
 }
