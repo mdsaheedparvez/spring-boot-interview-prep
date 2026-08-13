d185887e65bc1e71614e9602fc1e29395a5c4b46
Parvez <mdsaheedparvez95@gmail.com>
2026-08-14 01:31:23 +0530

    leetscode series day one

---
+++ src/leetCode/ArraysRelatedLeetcode/ContainsDuplicate.java
@@ -0,0 +1,69 @@
+package leetCode.ArraysRelatedLeetcode;
+
+import java.util.*;
+
+/*
+ * 217. Contains Duplicate
+Attempted
+Easy
+Topics
+premium lock icon
+Companies
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+
+Output: true
+
+Explanation:
+
+The element 1 occurs at the indices 0 and 3.
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+
+Output: false
+
+Explanation:
+
+All elements are distinct.
+
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+Output: true
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109*/
+
+public class ContainsDuplicate {
+
+	public static void main(String[] args) {
+		int nums[] = { 1, 2, 4, 5 };
+		boolean res = ContainsDuplicate.checkDuplicate(nums);
+		System.out.println("Contains duplicate: "+res);
+
+	}
+
+	private static boolean checkDuplicate(int[] nums) {
+		Map<Integer,Integer> map = new HashMap<>();
+		for(int i = 0 ; i < nums.length; i++) {
+			if(map.containsKey(nums[i])) {
+				return true;
+			}
+			map.put(nums[i],i);
+		}
+		return false;
+	}
+
+}
--- src/leetCode/StringRelatedLeetcode/LastWordcount.java
+++ src/leetCode/StringRelatedLeetcode/LastWordcount.java
@@ -1,4 +1,4 @@
-package leetCode;
+package leetCode.StringRelatedLeetcode;
 
 import java.util.Scanner;
 
